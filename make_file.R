7fde98893ba57f933fb7ea79180d2d06ac9e2fa5
galbraic <galbraic@uci.edu>
2018-02-01 16:38:48 -0800

    added comments

--- make_file.R
+++ make_file.R
@@ -1,8 +1,12 @@
+# grab all .Rmd files in workind directory
 files_to_render <- fs::dir_ls(glob = "*.Rmd")
+stopifnot(length(files_to_render) >= 1)
 
+# function to render .Rmd files to html that accepts date variable
 my_render <- function(x, d) {
   rmarkdown::render(here::here(x), 
                     params = list(reportdate=d)) 
 }
 
-purrr::walk(files_to_render, my_render, d="March 2, 2017")  # loop over all .Rmd files & render
+# loop over all .Rmd files & render with my function
+purrr::walk(files_to_render, my_render, d="March 2, 2017")  
